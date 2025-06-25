796964facdaa7033aa23574f57a876f037afd2c4
Said Rustom <105450621+SaidRustom@users.noreply.github.com>
2025-06-25 19:06:50 -0400

    Add MyAssistant.Identity (Duende)

---
+++ MyAssistant.Identity/Data/Migrations/20250625224356_1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAssistant.Identity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "AspNetUsers");
+        }
+    }
+}
